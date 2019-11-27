0dc9823ebcb35fd2af0c080a2680d7922742e33a
Alexey <Alexey@DESKTOP-5DT1SCD>
2019-11-27 17:05:38 +0200

    Add constvar class

---
+++ HomeTask/src/ua/dp/ort/homework3/ConstVar.java
@@ -0,0 +1,37 @@
+package ua.dp.ort.homework3;
+
+public class ConstVar {
+public static final float KATE = 7.77f;
+public static final int DIANA = 120;
+public static final long COOL = 23;
+public static final long ALEX = -85321;
+public static final double NASTY =4.5;
+	public static void main(String[] args) {
+		
+System.out.println(KATE);
+System.out.println(DIANA);
+System.out.println(COOL);
+System.out.println(ALEX);
+System.out.println(NASTY);
+
+float radius = 777.55f;
+System.out.println(KATE + radius );
+// ------------------------------------
+
+int discounts = -15578787;
+System.out.println(DIANA - discounts);
+//-------------------------------------
+
+long river = 7777777777777777777l;
+System.out.println(river - COOL);
+//-----------------------------------
+
+long credit = 1000000000l;
+System.out.println(credit - ALEX *100 );
+//------------------------------
+
+double weight¿pples = 33.2;
+System.out.println(weight¿pples - NASTY );
+	}
+
+}
