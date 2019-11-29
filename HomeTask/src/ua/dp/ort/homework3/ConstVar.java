369a4f613dd7c4e45aa6197d7b4fe0624d346846
Alexey <Alexey@DESKTOP-5DT1SCD>
2019-11-29 11:56:19 +0200

    Fix classes Bit, ConstVar, Data Types

--- HomeTask/src/ua/dp/ort/homework3/ConstVar.java
+++ HomeTask/src/ua/dp/ort/homework3/ConstVar.java
@@ -1,37 +1,20 @@
 package ua.dp.ort.homework3;
 
 public class ConstVar {
-public static final float KATE = 7.77f;
-public static final int DIANA = 120;
-public static final long COOL = 23;
-public static final double ALEX = 8532;
-public static final int NASTY =4;
-	public static void main(String[] args) {
-		
-System.out.println(KATE);
-System.out.println(DIANA);
-System.out.println(COOL);
-System.out.println(ALEX);
-System.out.println(NASTY);
+	
+public static final int WIDTH_CALCULATOR = 70;
+public static final long LENGTH_CALCULATOR = 150;
+public static final int LENGTH_BUTTON_CALCULATOR = 16;
+public static final float WIDTH_BUTTON_CALCULATOR  = 12.1f;
+public static final double AREA_WINDOWS_CALCULATOR =5460.3;
 
-float radius = 777.55f;
-System.out.println(KATE + radius );
-// ------------------------------------
-
-int discounts = -15578787;
-System.out.println(DIANA - discounts);
-//-------------------------------------
-
-long river = 7777777777777777777l;
-System.out.println(river - COOL);
-//-----------------------------------
-
-long credit = 1000000000l;
-System.out.println(credit - ALEX *100 );
-//------------------------------
-
-double weight¿pples = 33.2;
-System.out.println(weight¿pples - NASTY );
-	}
-
+public static void main(String[] args) {
+	System.out.println(WIDTH_CALCULATOR);
+	System.out.println(LENGTH_CALCULATOR);
+	System.out.println(LENGTH_BUTTON_CALCULATOR);
+	System.out.println( WIDTH_BUTTON_CALCULATOR);
+	System.out.println(AREA_WINDOWS_CALCULATOR);
 }
+}
+
+
