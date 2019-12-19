29c8f1a6458c498a17e8f1c73197eb6f71f5d0bf
Alexey <Alexey@DESKTOP-5DT1SCD>
2019-12-19 13:13:16 +0200

    Fix task2

--- HomeTask/src/ua/dp/ort/homework5/Homework5task2.java
+++ HomeTask/src/ua/dp/ort/homework5/Homework5task2.java
@@ -9,36 +9,37 @@ public class Homework5task2 {
 
 		Scanner scanner = new Scanner(System.in);
 
-	    String userInput;
+		String userInput;
 
-	    System.out.println("Input first number  ->");
-	    userInput = scanner.nextLine();
-	    double firstNumber = Double.parseDouble(userInput);
-	    System.out.println("Your first number  is " + userInput);
+		System.out.println("Input first number  ->");
+		userInput = scanner.nextLine();
+		double firstNumber = Double.parseDouble(userInput);
+		System.out.println("Your first number  is " + userInput);
 
-	    System.out.println("Input your second number ->");
-	    userInput = scanner.nextLine();
-	    double secondNumber = Double.parseDouble(userInput);
-	    System.out.println("Your second number is  " + userInput);
+		System.out.println("Input your second number ->");
+		userInput = scanner.nextLine();
+		double secondNumber = Double.parseDouble(userInput);
+		System.out.println("Your second number is  " + userInput);
 
-	    if (firstNumber == secondNumber) {
-	   System.out.println("Numbers are equal");
-	    } else {
-	        System.out.println("Numbers are different");
-	      }
-	  
-	      if (firstNumber > secondNumber) {
-	        System.out.println("First number a is greater than second number b");
-	      }
-	      if (firstNumber < secondNumber){
-	      System.out.println("First number a are less than second number b");
-	    }
-	  
-	      if (firstNumber >= secondNumber) {
-	        System.out.println("First number a is equal or greater second number b");
-	      } 
-	      if (firstNumber <= secondNumber){
-	    System.out.println("First number a are equal or less second number b");
-	    }
-	      scanner.close();
-	}}
+		if (firstNumber == secondNumber) {
+			System.out.println("Numbers are equal");
+		} else {
+			System.out.println("Numbers are different");
+		}
+
+		if (firstNumber > secondNumber) {
+			System.out.println("First number a is greater than second number b");
+		}
+		if (firstNumber < secondNumber) {
+			System.out.println("First number a are less than second number b");
+		}
+
+		if (firstNumber >= secondNumber) {
+			System.out.println("First number a is equal or greater second number b");
+		}
+		if (firstNumber <= secondNumber) {
+			System.out.println("First number a are equal or less second number b");
+		}
+		scanner.close();
+	}
+}
