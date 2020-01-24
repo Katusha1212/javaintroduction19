3701c7b868247698201e1b9738d10d834930bdd0
Alex <Alex@DESKTOP-R4EQ48D>
2020-01-24 17:33:32 +0200

    Add HomeWork 8

---
+++ HomeTask/src/ua/dp/ort/homework8/HomeWork8Task2.java
@@ -0,0 +1,38 @@
+package ua.dp.ort.homework8;
+
+import java.util.Scanner;
+
+public class HomeWork8Task2 {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int[] userNumbers = new int[20];
+
+		for (int counter = 0; counter < 20; counter++) {
+			System.out.print("Please input " + (counter + 1) + " number > ");
+
+			String userInput = scanner.nextLine();
+			int userNumber = Integer.parseInt(userInput);
+
+			userNumbers[counter] = userNumber;
+		}
+		scanner.close();
+
+		int pozitiveCount = 0;
+		double sum = 0;
+		double rezult = 0;
+
+		for (int counter = 0; counter < userNumbers.length; counter++) {
+			if (userNumbers[counter] > 0) {
+				sum = sum + userNumbers[counter];
+				pozitiveCount++;
+
+			}
+
+		}
+
+		rezult = sum / pozitiveCount;
+		System.out.println("Cеднее арифметическое положительных элементов " + rezult);
+
+	}
+}
---
+++ HomeTask/src/ua/dp/ort/homework8/HomeWork8Task3.java
@@ -0,0 +1,40 @@
+package ua.dp.ort.homework8;
+
+import java.util.Scanner;
+
+public class HomeWork8Task3 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] userNumbers = new int[20];
+
+		for (int counter = 0; counter < 20; counter++) {
+			System.out.print("Please input " + (counter + 1) + " number > ");
+
+			String userInput = scanner.nextLine();
+			int userNumber = Integer.parseInt(userInput);
+			userNumbers[counter] = userNumber;
+		}
+
+		scanner.close();
+		int min = userNumbers[0];
+		int max = userNumbers[0];
+		int counter = 0;
+
+		for (counter = 1; counter < userNumbers.length; counter++) {
+			if (min > userNumbers[counter]) {
+				min = userNumbers[counter];
+			}
+		}
+
+		for (counter = 1; counter < userNumbers.length; counter++) {
+			if (max < userNumbers[counter]) {
+				max = userNumbers[counter];
+			}
+		}
+		System.out.println("Минимальный элемент " + min);
+		System.out.println("Максимальный элемент " + max);
+
+	}
+
+}
